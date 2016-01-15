1a3d9e003d2fb7e71bf4db9e89544def34fdbc72
Mao Thygesen <maothygesen@gmail.com>
2016-01-15 09:55:33 -0800

    add var in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
-
+  $(".travelform").submit(function(){
+    $(".answer").empty();
+    var traveling_with = $("input [name=traveling-with]:checked").val();
+    var relax = $("input[name=vacation-type]:checked").val();
+    var season = $("input[name=season]:checked").val();
+    var money = $("input[name=money]:checked").val();
+    var length = $("input[name=length]:checked").val();
+    
+    if (traveling_with === "solo"){
+      if()
+    }
+  });
 
 });
