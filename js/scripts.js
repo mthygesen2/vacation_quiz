a5eaf4c42023f9a7f8a87fd4d6a25f6edd33feb1
Mao Thygesen <maothygesen@gmail.com>
2016-01-15 11:55:33 -0800

    try to figure out show function in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
-  $(".travelform").submit(function(){
-    $(".answer").empty();
-    var traveling_with = $("input [name=traveling-with]:checked").val();
-    var relax = $("input[name=vacation-type]:checked").val();
-    var season = $("input[name=season]:checked").val();
-    var money = $("input[name=money]:checked").val();
-    var length = $("input[name=length]:checked").val();
-    
-    if (traveling_with === "solo"){
-      if()
-    }
+  $("#travelform").submit(function(event){
+    // $("#countries").children("div").hide();
+    $("#countries").empty();
+    var traveling_with = $('#travelform input [name=with]:checked').val();
+    // var relax = $("input[name=vacation-type]:checked").val();
+    // var season = $("input[name=season]:checked").val();
+    // var money = $("input[name=money]:checked").val();
+    // var length = $("input[name=length]:checked").val();
+
+if (traveling_with === "solo"){
+//      if(season === "spring || summer"){
+//      if(length === "week || month"){
+      //  if(relax === "adventure"){
+         $("#countries").text("iceland").show();
+       } else{
+         $("#countries").text("he").show();
+      }
+ //   }
+ //   }
+ // }
+  event.preventDefault();
   });
 
 });
