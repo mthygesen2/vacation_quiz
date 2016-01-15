5265312daeae697df1597fe6e2966bb1e07bfcf8
Mao Thygesen <maothygesen@gmail.com>
2016-01-15 14:35:56 -0800

    return a working form

--- js/scripts.js
+++ js/scripts.js
@@ -7,47 +7,15 @@ $(document).ready(function(){
     var money = $("input[name=money]:checked").val();
     var length = $("input[name=length]:checked").val();
 
-    if(money === "small" || "medium"){
+    if(money === "small"){
       $(".country").text("Thailand, Cambodia or Laos");
-    } else {
-        $(".country").text("Iceland, Maldives, Switzerland");
-      }
-
+    }else {
+      $(".country").text("Iceland, Maldives, Switzerland");
+    }
       event.preventDefault();
+
     });
   });
-    // if(travelingwith === "solo") {
-    //
-    //     if(length === "month"){
-    //       $("country").text("Portland");
-    //     } else {
-    //       if(travelingwith ==== "family") {
-    //         $(".country").text("Seattle");
-    //       } else if(money === "issue" && relax === "adventure"){
-    //         $("country").text("Maldives, South Africa or Australia");
-    //       } else if(relax === "relax"){
-    //         $(".country").text("Indonesia, Japan or Brazil");
-    //       } else {
-    //         $(".country").text("Hawaii, Kenya or Ireland");
-    //       }
-    //     }
-    //   } else if (travelingwith === "couple") {
-    //     $(".country").text("Bali");
-    //   } else {
-    //     if(season === "winter"){
-    //       $(".country").text("Iceland, ");
-    //     } else {
-    //       if (length==="less" || "week"){
-    //         $(".country").text("Jamaica");
-    //       } else {
-    //         $(".country").text("Mongolia");
-    //       }
-    //     }
-    //   }
-    // }
-
-
-
 
 
 
@@ -59,13 +27,12 @@ $(document).ready(function(){
     // //       $(".country").text("Iceland, Switzerland or Antartica");
     // //     }
     // //   }
-    //     }else {
-    //     if(travelingwith === "solo"){
+
     //     if(relax === "adventure"){
     //       if(money === "small"){
     //         $(".country").text("Thailand, Cambodia or Mexico")
     //       }
-    //       else $(".country").text("France, Maldives or Australia");
+
     //     }
     //   }
     // }
