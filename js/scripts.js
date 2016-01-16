ee635f60e996a76138cbfe251a8cc2cce5a0ffe5
mthygesen2 <maothygesen@gmail.com>
2016-01-15 17:33:55 -0800

    fix js indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
-$(document).ready(function(){
-  $("form").submit(function(event){
-
-    var travelingwith = $('input[name="with"]:checked').val();
-    var relax = $("input[name=vacation-type]:checked").val();
-    var season = $("input[name=season]:checked").val();
-    var money = $("input[name=money]:checked").val();
-    var length = $("input[name=length]:checked").val();
-
-
-    if(money === "small") {
-      if(relax === "adventure") {
-        $(".country").text("Thailand, Cambodia or Laos");
-    }else {
-      if (money === "issue"){
-        $(".country").text("Antartica, Dubai or Japan");
-        }else {
-          if (season === "summer" && travelingwith === "solo"){
-            $(".country").text("Iceland, Maldives or  Switzerland");
-            }else {
+  $(document).ready(function(){
+    $("form").submit(function(event){
+  
+      var travelingwith = $('input[name="with"]:checked').val();
+      var relax = $("input[name=vacation-type]:checked").val();
+      var season = $("input[name=season]:checked").val();
+      var money = $("input[name=money]:checked").val();
+      var length = $("input[name=length]:checked").val();
+  
+  
+      if(money === "small") {
+        if(relax === "adventure") {
+          $(".country").text("Thailand, Cambodia or Laos");
+        } else {
+          if (money === "issue"){
+            $(".country").text("Antartica, Dubai or Japan");
+          } else {
+            if (season === "summer" || travelingwith === "solo"){
+              $(".country").text("Iceland, Maldives or Switzerland");
+            } else {
               $(".country").text("Turkey, Kenya or Brazil");
             }
           }
         }
-      } else
+      } else {
         $(".country").text("A staycation?");
+      }
+      
       event.preventDefault();
-
+  
     });
   });
 
